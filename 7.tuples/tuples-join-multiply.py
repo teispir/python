3f64b3a35850a10ebb3eaad51858b01592864737
Pierluigi Salera <igi_sweden@yahoo.it>
2023-07-27 09:16:21 +0200

    tuples exercises update

---
+++ 7.tuples/tuples-join-multiply.py
@@ -0,0 +1,14 @@
+# join tuples
+
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+# multiply tuples
+
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
